be297d3f9240b162071e75c79f46f4a3aa6fe4e4
Lsze5821 <46796206+Lsze5821@users.noreply.github.com>
2019-09-30 17:41:04 -0400

    Added Interface

---
+++ src/com/company/Chassis.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface Chassis {
+  public String chassis = "Chassis";
+  public Chassis getChassisType();
+  public void setChassisType(String vehicleChassis);
+
+
+}
